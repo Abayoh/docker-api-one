2515c4ef2e712c39663df0588ebd217d81cf0aa9
“abayoh” <alexanderbayoh@hotmail.com>
2022-09-21 18:21:48 +0000

    edited the code

--- index.js
+++ index.js
@@ -23,4 +23,6 @@ app.get('/', (req, res) => {
   ]);
 });
 
-app.listen(5000, () => console.log('Server started on port 5000'));
+const port = process.env.PORT || 8080;
+
+app.listen(port, () => console.log('Server started on port 8080'));
